739817da5c20a6379814cdc7bad68ca9bb91eec9
Tiago Pavani <tiagopavanisalto@hotmail.com>
2022-08-28 09:08:46 -0300

    Add function de limpar o form

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const submitBtn = document.getElementById("submitBtn")
+const inputEmail = document.getElementById("inputEmail")
+const inputSenha = document.getElementById("inputPassword")
 
 function avisoDeFormEnviado(){
     alert("Dados Enviados.")
+}
+
+function clearForm(){
+    submitBtn.addEventListener("click")
+    inputEmail.innerHTML= "";
+    inputSenha.innerHTML= "";
 }
