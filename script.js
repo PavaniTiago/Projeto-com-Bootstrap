19297ecb3e6e65c9c7d328fdb69ba2ff65620f2d
Tiago Pavani <tiagopavanisalto@hotmail.com>
2022-08-29 10:09:16 -0300

    Novas atualições no js

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ const submitBtn = document.getElementById("submitBtn")
 const inputEmail = document.getElementById("inputEmail")
 const inputSenha = document.getElementById("inputPassword")
 
-function avisoDeFormEnviado(){
-    alert("Dados Enviados.")
-}
 
 function clearForm(){
     submitBtn.addEventListener("click")
     inputEmail.innerHTML= "";
     inputSenha.innerHTML= "";
+}
+
+function validarForm(){
+    if(inputEmail.value == ""){
+        alert("Insira os dados corretamente.")
+        inputEmail.focus();
+        return false;
+    }
+    else if(inputSenha.value == ""){
+        alert("Insira os dados corretamente")
+        inputSenha.focus();
+        return false;
+    }else{
+        alert("Dados insiridos corretamente.")
+        clearForm()
+        return true
+    }
 }
